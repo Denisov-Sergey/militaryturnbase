d7622b8e72e69e1fc8eeb6da573e61439ecdec0b
Jay <sithis206@gmail.com>
2025-03-07 23:02:28 +0600

    зафиксирована камера

--- Assets/MilitaryTurnBase/Scripts/Core/IsometricCameraController.cs
+++ Assets/MilitaryTurnBase/Scripts/Core/IsometricCameraController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
  */
 public class IsometricCameraController : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, 0f);
+    [SerializeField] private float smothSpeed = 1f;
+    
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float zoomSpeed = 5f;
     [SerializeField] private float minZoom = 5f;
@@ -12,6 +16,12 @@ public class IsometricCameraController : MonoBehaviour
     
     void LateUpdate()
     {
+       Vector3 targetPosition = playerTransform.position + offset;
        
+       Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smothSpeed);
+       
+       transform.position = smoothedPosition;
+       
+       // transform.LookAt(playerTransform);
     }
 }
